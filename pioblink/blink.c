03e2f8c7109563983c3bb841fc6c86e07db56ba4
alanbontoft <alan.bontoft@gmail.com>
2024-07-19 13:23:06 +0100

    Initial source commit

---
+++ pioblink/blink.c
@@ -0,0 +1,42 @@
+/**
+ * Copyright (c) 2020 Raspberry Pi (Trading) Ltd.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include "pico/stdlib.h"
+#include "hardware/pio.h"
+// Our assembled program:
+#include "blink.pio.h"
+
+int main() {
+#ifndef PICO_DEFAULT_LED_PIN
+#warning pio/hello_pio example requires a board with a regular LED
+#else
+    // Choose which PIO instance to use (there are two instances)
+    PIO pio = pio0;
+
+    // Our assembled program needs to be loaded into this PIO's instruction
+    // memory. This SDK function will find a location (offset) in the
+    // instruction memory where there is enough space for our program. We need
+    // to remember this location!
+    uint offset = pio_add_program(pio, &pioblink_program);
+
+    // Find a free state machine on our chosen PIO (erroring if there are
+    // none). Configure it to run our program, and start it, using the
+    // helper function we included in our .pio file.
+    uint sm = pio_claim_unused_sm(pio, true);
+    pioblink_program_init(pio, sm, offset, PICO_DEFAULT_LED_PIN);
+
+    // The state machine is now running. Any value we push to its TX FIFO will
+    // appear on the LED pin.  Pin 26 follows LED pin, Pin 27 is inverted
+    while (true) {
+        // Blink
+        pio_sm_put_blocking(pio, sm, 0x00000003);
+        sleep_ms(100);
+        // Blonk
+        pio_sm_put_blocking(pio, sm, 0x00000004);
+        sleep_ms(50);
+    }
+#endif
+}
